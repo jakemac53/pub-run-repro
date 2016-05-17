6301df01c3c38d7e90c0e81ad3b6997263a27f7d
Jacob MacDonald <jakemac@google.com>
2016-05-17 07:26:23 -0700

    initial commit

---
+++ b/lib/b.dart
@@ -0,0 +1,1 @@
+final String message = 'hello world';
---
+++ test/a_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+import 'package:b/b.dart';
+
+main() {
+  test('hello world', () {
+    expect(message, 'hello world');
+  });
+}
